f7ab49cb39fc2d5bb210762be0df3e7170b99a13
Ajay <ajayjogdand374@gmail.com>
2025-04-22 07:52:43 +0530

    fix:Bugs

--- tagsiftpharmacy/tagsiftpharmacy/doctype/user_details/user_details.py
+++ tagsiftpharmacy/tagsiftpharmacy/doctype/user_details/user_details.py
@@ -7,18 +7,22 @@ from frappe.model.document import Document
 
 class UserDetails(Document):
 	def after_insert(self):
-		doc=frappe.new_doc("User")
-		doc.email=self.email
-		doc.first_name=self.email
-		doc.username=self.email
-		doc.mobile_no=self.phone_number
-		doc.phone=self.phone_number
-		doc.user_type="Website User"
-		doc.append("roles",{
-			"role":"Pharmacy User"
-		})
-		doc.save(ignore_permissions=True)
-		self.db_set("user",doc.name)
+		user=frappe.db.get_value("User",self.user,"name")
+		if user:
+			self.db_set("user",user)
+		if not user:
+			doc=frappe.new_doc("User")
+			doc.email=self.email
+			doc.first_name=self.email
+			doc.username=self.email
+			doc.mobile_no=self.phone_number
+			doc.phone=self.phone_number
+			doc.user_type="Website User"
+			doc.append("roles",{
+				"role":"Pharmacy User"
+			})
+			doc.save(ignore_permissions=True)
+			self.db_set("user",doc.name)
 
 	def before_save(self):
 		if self.user:
